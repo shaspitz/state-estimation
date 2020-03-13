aa4f90651fda19a50d93556ef91a5cb4238ea9ee
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2020-03-13 15:15:20 -0700

    problem setup

--- ME231B_Assignment_5/problem_4.py
+++ ME231B_Assignment_5/problem_4.py
@@ -5,3 +5,19 @@ Created on Mar 13, 2020
 
 UC Berkeley ME231B Assignment #5 Problem 4
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Implement time-varying linear system as given in problem statement
+n = 2
+A = np.array([[0.8, 0.6], [-0.6, 0.8]])
+H = np.array([1, 0])
+
+# Initial prior estimate and prior covariance of state
+xp, Pp = 0, np.array([[3, 0], [0, 1]])
+
+# Time-invariant process noise and measurement noise given as identity
+V, W = np.eye(n, dtype = int), np.eye(n, dtype = int)
+
+print(W)
