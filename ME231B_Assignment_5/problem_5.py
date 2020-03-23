ee5aa3e113ce4c6a51c4722d8e6e607f25ff31d9
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2020-03-23 15:57:34 -0700

    prob5 finished

--- ME231B_Assignment_5/problem_5.py
+++ ME231B_Assignment_5/problem_5.py
@@ -81,19 +81,17 @@ for k in range(1, T_f):  # Note that estimate for k=0 is initialized above
     P_est[k] = Pm
 
 #  Plotting
-num_bins = 20
-
 plt.figure(0)
-plt.plot(range(0, T_f), x_est)
+plt.plot(range(0, T_f), x_est, linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Estimated Volume of Water, r')
-plt.title(r'Estimated Volume of Water, r vs Time (Part a)')
+plt.ylabel('Estimated Volume of Water')
+plt.title(r'Estimated Volume of Water vs Time (Part a)')
 
 plt.figure(1)
-plt.plot(range(0, T_f), P_est)
+plt.plot(range(0, T_f), P_est, linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Uncertainty of Estimated Volume of Water, r')
-plt.title(r'Uncertainty of Estimated Volume of Water, r vs Time (Part a)')
+plt.ylabel('Uncertainty of Estimated Volume of Water')
+plt.title(r'Uncertainty of Estimated Volume of Water vs Time (Part a)')
 
 # Part (b)
 
@@ -118,28 +116,26 @@ def time_update_b(xm, Pm):
     # Known input, d added to system
     xp = A @ xm + np.array([[d], [0]])
     # Process noise only affects second component of state below
-    Pp = A @ Pm @ A.transpose() + np.array([[0], [V]])
+    Pp = A @ Pm @ A.T + np.array([[0], [V]])
     return xp, Pp
 
 
 def meas_update_b(xp, Pp, z):
-    K = Pp @ H.transpose() @ np.linalg.inv(H @ Pp @ H.transpose() + W)
+    K = Pp @ H.T @ np.linalg.inv(H @ Pp @ H.T + W)
     xm = xp + K @ (z - H @ xp)
-    Pm = (np.eye(N) - K @ H) @ Pp @ np.transpose(np.eye(N) - K @ H)
-    + K * W * K.transpose()
+    Pm = (np.eye(N) - K @ H) @ Pp @ (np.eye(N) - K @ H).T
+    + K * W * K.T
     return xm, Pm
 
 
 # Initialize estimate and covariance of state (at k = 0)
 xm, Pm = np.array([[20], [7]]), np.array([[25, 0], [0, 1]])
 
-T_f = 11  # Simulation Timesteps (0 included)
-
 # preallocate parameters
-x_est = np.zeros((T_f, 2))
+x_est = np.zeros((T_f, N))
 x_est[0] = xm.ravel()
-P_est = np.zeros((T_f, 4))
-P_est[0] = Pm.ravel()
+P_est = np.zeros((T_f, N))
+P_est[0] = Pm.diagonal()
 
 # Given measurements are same as part (a)
 
@@ -153,40 +149,233 @@ for k in range(1, T_f):  # Note that estimate for k=0 is initialized above
 
     # Store results for plotting
     x_est[k] = xm.ravel()
-    P_est[k] = Pm.ravel()
+    P_est[k] = Pm.diagonal()
 
 #  Plotting
-num_bins = 20
-
 plt.figure(2)
-plt.plot(range(0, T_f), x_est[:, 0])
+plt.plot(range(0, T_f), x_est[:, 0], linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Estimated Volume of Water, r')
-plt.title(r'Estimated Volume of Water, r vs Time (Part b)')
+plt.ylabel('Estimated Volume of Water')
+plt.title(r'Estimated Volume of Water vs Time (Part b)')
 
 plt.figure(3)
-plt.plot(range(0, T_f), x_est[:, 1])
+plt.plot(range(0, T_f), x_est[:, 1], linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Estimated Consumption of Water, c')
-plt.title(r'Estimated Consumption of Water, c vs Time (Part b)')
+plt.ylabel('Estimated Consumption of Water')
+plt.title(r'Estimated Consumption of Water vs Time (Part b)')
 
 plt.figure(4)
-plt.plot(range(0, T_f), P_est[:, 0])
+plt.plot(range(0, T_f), P_est[:, 0], linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Uncertainty of Estimated Volume of Water, r')
-plt.title(r'Uncertainty of Estimated Volume of Water, r vs Time (Part b)')
+plt.ylabel('Uncertainty of Estimated Volume of Water')
+plt.title(r'Uncertainty of Estimated Volume of Water vs Time (Part b)')
 
 plt.figure(5)
-plt.plot(range(0, T_f), P_est[:, 3])
+plt.plot(range(0, T_f), P_est[:, 1], linewidth=3)
 plt.xlabel('Timestep (k)')
-plt.ylabel('Uncertainty of Estimated Consumption of Water, c')
-plt.title(r'Uncertainty of Estimated Consumption of Water, c vs Time (Part b)')
+plt.ylabel('Uncertainty of Estimated Consumption of Water')
+plt.title(r'Uncertainty of Estimated Consumption of Water vs Time (Part b)')
 
 # Part (c)
 
+# First define global vars
+
+# Time-invariant linear system deduced from problem statement
+N = 8
+d = 30  # constant supply of water d(k)
+alph = 0.3  # Flow balancing of 0.3
+
+A = np.array([[1-2*alph, alph, alph, 0, -1, 0, 0, 0],
+              [alph, 1-2*alph, alph, 0, 0, -1, 0, 0],
+              [alph, alph, 1-2*alph, 0, 0, 0, -1, 0],
+              [0, 0, alph, 1-alph, 0, 0, 0, -1],
+              [0, 0, 0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0, 1]])
+
+H = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0, 0, 0]])
+
+# Time-invariant process and measurement noise covariances
+V = 0.1 * np.eye(N)
+V[0:4, 0:4] = 0  # Adjust V so process noise only affects last four states
+W = 25 * np.eye(4)  # z(k) is of size 4
+
+# Process and measurement noise are zero mean, white, and independant
+
+# Define measurement and time update for Kalman Filter implementation
 
 
+def time_update_c(xm, Pm):
+    xp = A @ xm
+    Pp = A @ Pm @ A.T + V
+    return xp, Pp
+
+
+def meas_update_c(xp, Pp, z):
+    K = Pp @ H.T @ np.linalg.inv(H @ Pp @ H.T + W)
+    xm = xp + K @ (z - H @ xp)
+    Pm = (np.eye(N) - K @ H) @ Pp @ (np.eye(N) - K @ H).T
+    + K @ W @ K.T
+    return xm, Pm
+
+
+# Initialize estimate and covariance of state (at k = 0)
+xm = np.array([[20, 40, 60, 20, 7, 7, 7, 7]]).T
+Pm = np.diag([20, 20, 20, 20, 1, 1, 1, 1])
+
+# preallocate parameters
+x_est = np.zeros((T_f, N))
+x_est[0] = xm.ravel()
+P_est = np.zeros((T_f, N))
+P_est[0] = Pm.diagonal()
+
+# Given measurements
+z = np.array([[62.6, 70.3, 73.5, 77.2, 73.2, 94.2, 87.4, 89.7, 90.4, 94.2],
+              [29.4, 44.8, 37.3, 40.1, 44.1, 43.8, 53.8, 49.9, 51.9, 52.1],
+              [35.9, 38.8, 25.9, 39.0, 31.2, 46.9, 39.6, 44.0, 42.5, 54.2],
+              [40.9, 21.9, 18.0, 08.8, 23.9, 17.2, 18.6, 22.0, 22.9, 17.2]])
+
+for k in range(1, T_f):  # Note that estimate for k=0 is initialized above
+
+    # Kalman filter estimation: time update
+    xp, Pp = time_update_c(xm, Pm)
+
+    # Kalman filter estimation: measurement update
+    # (ensuring z(k) is 4x1 after slicing)
+    xm, Pm = meas_update_c(xp, Pp, z[0:4, k-1].reshape(4, 1))
+
+    # Store results for plotting
+    x_est[k] = xm.ravel()
+    P_est[k] = Pm.diagonal()
+
+#  Plotting
+plt.figure(6)
+plt.plot(range(0, T_f), x_est[:, 0:4], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Estimated Volume of Water')
+plt.title(r'Estimated Volume of Water vs Time (Part c)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="upper center")
+
+plt.figure(7)
+plt.plot(range(0, T_f), x_est[:, 4:9], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Estimated Consumption of Water')
+plt.title(r'Estimated Consumption of Water vs Time (Part c)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="lower left")
+
+linestyle = ['-.', '--', ':', '-']
+plt.figure(8)
+for k in range(0, 4):
+    plt.plot(range(0, T_f), P_est[:, k], linestyle=linestyle[k], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Uncertainty of Estimated Volume of Water')
+plt.title(r'Uncertainty of Estimated Volume of Water '
+          'vs Time (Part c)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="upper right")
+
+linestyle = ['-', '--', ':', '-']
+plt.figure(9)
+for k in range(4, 8):
+    plt.plot(range(0, T_f), P_est[:, k], linestyle=linestyle[k-4], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Uncertainty of Estimated Consumption of Water')
+plt.title(r'Uncertainty of Estimated Consumption of Water '
+          'vs Time (Part c)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="upper right")
+
+print(
+    'The estimate uncertainty for tank 1 is lower than the solution we had'
+    ' for the simpler network because the system dynamics bewteen tanks are'
+    ' coupled. Therefore the additional measurements of added states reduce '
+    'the estimate uncertainty of tank 1.')
+
+# Part c-iii, repeat part c with tank 3 sensor inactive
+
+# N, d, alpha, and A are same as before, H is redefined below
+H = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0, 0, 0]])
+
+# Process noise is the same as before, measurement noise redefined below
+W = 25 * np.eye(3)  # z(k) is of size 3
+
+# Process and measurement noise are zero mean, white, and independant
+
+# Initialize estimate and covariance of state (at k = 0)
+xm = np.array([[20, 40, 60, 20, 7, 7, 7, 7]]).T
+Pm = np.diag([20, 20, 20, 20, 1, 1, 1, 1])
+
+# preallocate parameters
+x_est = np.zeros((T_f, N))
+x_est[0] = xm.ravel()
+P_est = np.zeros((T_f, N))
+P_est[0] = Pm.diagonal()
+
+# Given measurements (tank 3 sensor removed)
+z = np.array([[62.6, 70.3, 73.5, 77.2, 73.2, 94.2, 87.4, 89.7, 90.4, 94.2],
+              [29.4, 44.8, 37.3, 40.1, 44.1, 43.8, 53.8, 49.9, 51.9, 52.1],
+              [40.9, 21.9, 18.0, 08.8, 23.9, 17.2, 18.6, 22.0, 22.9, 17.2]])
+
+for k in range(1, T_f):  # Note that estimate for k=0 is initialized above
+
+    # Kalman filter estimation: time update
+    xp, Pp = time_update_c(xm, Pm)
+
+    # Kalman filter estimation: measurement update
+    # (ensuring z(k) is 3x1 after slicing)
+    xm, Pm = meas_update_c(xp, Pp, z[0:3, k-1].reshape(3, 1))
+
+    # Store results for plotting
+    x_est[k] = xm.ravel()
+    P_est[k] = Pm.diagonal()
+
+#  Plotting
+plt.figure(10)
+plt.plot(range(0, T_f), x_est[:, 0:4], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Estimated Volume of Water')
+plt.title(r'Estimated Volume of Water vs Time (Part c-iii)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="upper center")
+
+plt.figure(11)
+plt.plot(range(0, T_f), x_est[:, 4:9], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Estimated Consumption of Water')
+plt.title(r'Estimated Consumption of Water vs Time (Part c-iii)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="lower left")
+
+linestyle = ['-', '--', '-', '-']
+plt.figure(12)
+for k in range(0, 4):
+    plt.plot(range(0, T_f), P_est[:, k], linestyle=linestyle[k], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Uncertainty of Estimated Volume of Water')
+plt.title(r'Uncertainty of Estimated Volume of Water '
+          'vs Time (Part c-iii)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="upper right")
+
+linestyle = ['-', '--', '-', '-']
+plt.figure(13)
+for k in range(4, 8):
+    plt.plot(range(0, T_f), P_est[:, k], linestyle=linestyle[k-4], linewidth=3)
+plt.xlabel('Timestep (k)')
+plt.ylabel('Uncertainty of Estimated Consumption of Water')
+plt.title(r'Uncertainty of Estimated Consumption of Water '
+          'vs Time (Part c-iii)')
+plt.legend(labels=['Tank 1', 'Tank 2', 'Tank 3', 'Tank 4'], loc="right")
+
+print('losing this sensor has slightly affected our ability to estimate'
+      ' the tank levels. As seen by comparing the graphs bewteen (c) and'
+      ' (c-iii), Losing the tank 3 sensor moderately increased the uncertainty'
+      ' for the estimated consumption and volume of tank 3 only. Uncertainties'
+      ' for the remaining 6 states remained relatively unchanged. Estimation'
+      ' graphs were affected by removing the sensor, but their overall shapes'
+      ' remained consistent. ')
 
 plt.show()
-print(0)
 
+print(0)
