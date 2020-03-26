99a8abb32fdfecd07b7afc4a02a635755d65a4eb
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2020-03-26 14:58:03 -0700

    prob3

--- ME231B_Assignment_6/problem_3.py
+++ ME231B_Assignment_6/problem_3.py
@@ -25,7 +25,7 @@ A = np.array([[1, dt], [0, 1]])
 H = np.array([[1, 0]])
 
 
-def noise_orig_sys(dt, m1):
+def noise_orig_sys():
     # Process and measurement noise covariances
     return stdev**2*np.array([[1/4*dt**4, 1/2*dt**3], [1/2*dt**3, dt**2]]), m1**2
     # Noises are zero mean, gaussian, and independant
@@ -53,7 +53,7 @@ def compute_jp():
     return np.sqrt(Pm_inf[0, 0])
 
 
-V, W = noise_orig_sys(dt, m1)
+V, W = noise_orig_sys()
 print('The original system has Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
 
 '''
@@ -68,7 +68,7 @@ system, compute the performance metric Jp
 (the noise standard deviation is cut in half).
 '''
 m1 = 5
-V, W = noise_orig_sys(dt, m1)
+V, W = noise_orig_sys()
 print('For part (b) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
 
 '''
@@ -77,7 +77,8 @@ and m1 = 10m (i.e. the sensor returns equally noisy measurements,
  but twice as frequently).
 '''
 dt, m1 = 1/20, 10
-V, W = noise_orig_sys(dt, m1)
+A = np.array([[1, dt], [0, 1]])
+V, W = noise_orig_sys()
 print('For part (c) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
 
 '''
@@ -87,11 +88,11 @@ white noise sequence, independent of all quantities, and with
 Var [w2(k)] = m2 ** 2 with m2 = 1m/s. The sensors z1 and z2 return data
 at the same instants of time.
 '''
-H = np.eye(N)
-m2 = 1
+dt, m1, m2, H = 1/10, 10, 1, np.eye(N)
+A = np.array([[1, dt], [0, 1]])
 
 
-def noise_part_d(dt, m1, m2):
+def noise_part_d():
     # Process and measurement noise covariances
     V = stdev**2*np.array([[1/4*dt**4, 1/2*dt**3], [1/2*dt**3, dt**2]])
     W = np.array([[m1**2, 0], [0, m2**2]])
@@ -99,6 +100,43 @@ def noise_part_d(dt, m1, m2):
     # Noises are zero mean, gaussian, and independant
 
 
-V, W = noise_part_d(dt, m1, m2)
+V, W = noise_part_d()
 print('For part (d) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
+
+'''
+(e) Retain the original GPS sensor, and add a second, independent barometric
+height sensor z2(k) = x1(k) + w2(k), where w2(k) is a zero-mean, white noise
+sequence, independent of all quantities, and with Var [w2(k)] = m2 ** 2
+with m2 = 10m. The sensors z1 and z2 return data at the same instants of
+time.
+'''
+m2, H = 10, np.array([[1, 0], [1, 0]])
+V, W = noise_part_d()
+print('For part (e) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
+
+'''
+(f) Retain the original GPS sensor, and add a second identical GPS sensor from
+the same manufacturer, so that z2(k) = x1(k) + w2(k), where w2(k) = w1(k).
+'''
+
+
+def noise_part_f():
+    # Process and measurement noise covariances
+    V = stdev**2*np.array([[1/4*dt**4, 1/2*dt**3], [1/2*dt**3, dt**2]])
+    W = m1**2*np.array([[1.0001, 1], [1, 1.0001]])
+    return V, W
+
+
+V, W = noise_part_f()
+print('For part (f) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
+
+'''
+(g) Retain the original sensor, but modify the system design by making
+it more aerodynamic and thereby reducing the effect of aerodynamic disturbances,
+so that stdev = 1m/s.
+'''
+
+stdev, H = 1, np.array([[1, 0]])
+V, W = noise_orig_sys()
+print('For part (g) Jp ~= ' + repr(round(compute_jp(), 3)) + 'm')
 
